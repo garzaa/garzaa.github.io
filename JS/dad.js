d8053c8c556f819b6e7fd571b121bda425d17fe1
Adrian Garza <adrianhgarza@gmail.com>
2015-09-08 11:26:29 -0400

    center canvas, add title

--- JS/dad.js
+++ JS/dad.js
@@ -4,7 +4,7 @@ var ctx = canvas.getContext("2d");
 var entryway = document.getElementById("entryway");
 
 function drawEntryway() {
-	ctx.drawImage(entryway, 0, 0, 800, 400) //img,x,y,width,height
+	ctx.drawImage(entryway, 0, 0, 800, 600) //img,x,y,width,height
 }
 
 //before gamecode
