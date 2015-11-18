133fb2ec41951e212e119034b3130684565cc26d
adriangarza <adrianhgarza@gmail.com>
2015-11-17 22:51:28 -0500

    on hold till i figure out rendering stats

--- dm/dmscript.js
+++ dm/dmscript.js
@@ -1,11 +1,36 @@
-//dice
+//global helpers
 function d(sides) {	
 	return Math.floor(Math.random() * sides) + 1
 }
 
+function render(obj) {
+	var output
+	var preWrapper = "<p id=" + obj.id + ">"
+	var postWrapper = "</p>"
+	$("#enemies").prepend(output)
+}
+
 //enemy objects
-function lap()
+
+//lightly armored pirate
+function lap() {
+	laps++
+	this.id = "lap" + laps
+
+	this.hp = 10 + d(6)
+	this.ac = 8
+	this.str = 2
+	this.dex = 0
+	this.con = 3
+	this.wis = 0
+	this.cha = 0
+	this.sp = 30
+	function dmg() {
+		this.hp =- 1
+	}
+	render(this)
+}
 
 $("#lap").click(function() {
-	createLap()
+	lap()
 })
