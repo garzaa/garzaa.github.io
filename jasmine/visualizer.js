513d5f6c86248d5295ca2bab2a57e137b2056ddd
adriangarza <adrianhgarza@gmail.com>
2016-02-11 00:05:08 -0500

    new working music player

---
+++ jasmine/visualizer.js
@@ -0,0 +1,11 @@
+//drawing the inital state
+var canvas = document.getElementById("visualizer");
+var ctx = canvas.getContext("2d");
+
+//drawing test
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, 500, 500);
+
+function renderFrame() {
+
+}
