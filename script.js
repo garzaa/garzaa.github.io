782ea3cfea287ed7f74d96f70ea8fa2cc4302d51
adriangarza <adrianhgarza@gmail.com>
2017-02-20 00:30:32 -0500

    removed extra files, added writings

--- script.js
+++ script.js
@@ -14,26 +14,30 @@ $("#about").click(function() {
     updateTabs("about");
 })
 
+$("#writing").click(function() {
+    updateTabs("writing");
+})
+
 function hideAll() {
-    $("#appslist,#gameslist,#personallist,#aboutlist").hide();
+    $("#appslist,#gameslist,#personallist,#writinglist,#aboutlist").hide();
 }
 
 function updateTabs(input) {
     hideAll()
-    $("#apps,#games,#personal,#about").removeClass("activetab");
+    $("#apps,#games,#personal,#writing,#about").removeClass("activetab");
     //show the list
     $("#" + input + "list").fadeIn(200);
     currentList = input;
 
     //update the css for the tabs
     $("#" + input).addClass("activetab");
-    
+
     var tmpWidth = $("#" + input).width();
     var tmpPos = $("#" + input).offset().left;
-    
+
     $("#line").css("width", tmpWidth + "px");
     $("#line").css("margin-left", tmpPos + "px");
-    
+
 }
 
 updateTabs("about");
