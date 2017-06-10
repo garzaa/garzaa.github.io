dc251b74f49aacd710fe4029e2d12e925c5ab998
adriangarza <adrianhgarza@gmail.com>
2017-06-10 16:00:26 -0700

    tentative jasmine mixtape

---
+++ jasmine/script.js
@@ -0,0 +1,298 @@
+var currentSong;
+var playing;
+var firstClick = true;
+var track = 0;
+var imageToggled;
+var timerUpdater;
+var smoothTransition = true;
+var cachedVolume;
+
+document.head = document.head || document.getElementsByTagName('head')[0];
+
+function changeFavicon(src) {
+ var link = document.createElement('link'),
+     oldLink = document.getElementById('dynamic-favicon');
+ link.id = 'dynamic-favicon';
+ link.rel = 'shortcut icon';
+ link.href = src;
+ if (oldLink) {
+  document.head.removeChild(oldLink);
+ }
+ document.head.appendChild(link);
+}
+
+function play(song, update) {
+    currentSong.pause();
+    currentSong.currentTime = 0;
+    currentSong = new Audio(song.audioPath);
+    currentSong.play();
+    if (firstClick) {
+        firstClick = false;
+    }
+
+    clearInterval(timerUpdater);
+    timerUpdater = setInterval(function() {
+        updateTime(currentSong)
+    }, 50);
+
+    $("#playIcon").text("pause")
+    firstClick = false;
+    playing = true;
+    track = songs.indexOf(song)
+
+    addSongInfo(song)
+
+    currentSong.onended = function() {
+        next();
+    }
+
+    if (!update) {
+        setAlbumArt(song.artPath);
+    }
+}
+
+function addSongInfo(song) {
+    $("#songinfo").html(
+        "<span class='song-name'>" + song.name +
+        "</span><br> <a target='_blank' href='" + song.link + "'>" +
+        song.artist + "</a>"
+    )
+
+    document.title = song.name +  " - " + song.artist
+
+    setBgArt(song.artPath);
+    changeFavicon(song.artPath);
+}
+
+function setBgArt(imgPath) {
+    //$("#bg-image").attr("src", imgPath);
+    if (!imageToggled) {
+        $("#new-bg").attr("src", imgPath);
+        $("#new-bg").css("opacity", .2);
+        $("#bg-image").css("opacity", 0);
+    } else {
+        $("#bg-image").attr("src", imgPath);
+        $("#bg-image").css("opacity", .2);
+        $("#new-bg").css("opacity", 0);        
+    }
+    imageToggled = !imageToggled;
+}
+
+$('body').keyup(function(e){
+   if(e.keyCode == 32 || e.keyCode == 75){
+       //space or k
+       e.preventDefault();
+       pause();
+   } else if (e.keyCode == 39) {
+       //right arrow
+        e.preventDefault();
+        next()
+   } else if (e.keyCode == 37) {
+       e.preventDefault();
+       prev();
+   } else if (e.keyCode == 74) {
+        //j
+        currentSong.currentTime -= 10;
+   } else if (e.keyCode == 76) {
+        //l
+        currentSong.currentTime += 10;
+   }
+});
+
+$('body').keydown(function(e){
+   if(e.keyCode == 32){
+       // doh ho ho
+       e.preventDefault();
+   }
+});
+
+function seek(e) {
+    var percent = e.offsetX / this.offsetWidth;
+    currentSong.currentTime = percent * currentSong.duration;
+}
+
+function pause() {
+    if (firstClick === true) {
+        play(songs[0]);
+        firstClick = false;
+        $("#playIcon").text("pause")
+    } else if (playing) {
+        currentSong.pause();
+        playing = false;
+        $("#playIcon").text("play_arrow")
+    } else {
+        currentSong.play();
+        playing = true;
+        $("#playIcon").text("pause")
+    }
+}
+
+function next() {
+    //loop if at the end
+    var toPlay;
+    var wasPlaying = playing;
+    if (track != songs.length-1) {
+        toPlay = songs[track + 1];
+    } else { toPlay = songs[0]; }
+    play(toPlay, true);
+    if (!wasPlaying) pause();
+    
+    //if there's time, do a smooth transtiion
+    if (smoothTransition) {
+        $("#new-art").attr("src", toPlay.artPath);
+        $("#current-art").addClass("fadeLeft");
+        $("#new-art").addClass("fadeRight");
+        smoothTransition = false;
+        setTimeout(function() {
+            $("#current-art").removeClass("fadeLeft");
+            $("#new-art").removeClass("fadeRight");
+            $("#current-art").attr("src", toPlay.artPath);
+            smoothTransition = true;
+        }, 500) 
+    } else {
+        $("#current-art").attr("src", toPlay.artPath);
+        $("#new-art").attr("src", toPlay.artPath);
+        smoothTransition = false;
+        setTimeout(function() {smoothTransition = true}, 500)
+    }
+
+}
+
+function prev() {
+    var toPlay;
+    var wasPlaying = playing;
+    if (currentSong.currentTime > 3) { //rewinds if in the middle of a song
+        currentSong.currentTime = 0;
+        return;
+    } else if (track === 0) {
+            toPlay = (songs[songs.length-1]);
+    } else { toPlay = (songs[track - 1]); }
+    play(toPlay, true);
+    if (!wasPlaying) pause();
+
+    if (smoothTransition) {
+        $("#new-art").attr("src", toPlay.artPath);
+        $("#current-art").addClass("fadeRight");
+        $("#new-art").addClass("fadeLeft");
+        smoothTransition = false;
+        setTimeout(function() {
+            $("#current-art").removeClass("fadeRight");
+            $("#new-art").removeClass("fadeLeft");
+            $("#current-art").attr("src", toPlay.artPath);
+            smoothTransition = true;
+        }, 500)
+    } else {
+        $("#current-art").attr("src", toPlay.artPath);
+        $("#new-art").attr("src", toPlay.artPath);
+        smoothTransition = false;
+        setTimeout(function() {smoothTransition = true}, 500)
+    }
+}
+
+function updateTrackbar() {
+    var percent = (currentSong.currentTime / currentSong.duration) * 100
+    percent += "%"
+    $("#trackbar").css("width", percent)
+}
+
+function updateTime(song) {
+    if (!song.playing || song.currentTime == "NaN") return;
+    $("#time").text(
+        formatTime(song.currentTime) + "/" + formatTime(song.duration)
+    );
+}
+
+$(document).ready(function() {
+    setInterval(updateTrackbar, 20)
+
+    currentSong = new Audio(songs[0].audioPath);
+
+    addSongInfo(songs[0]);
+    setAlbumArt(songs[0].artPath);
+
+    for (var i=0; i<songs.length; i++) {
+        $("#tracklist").append(
+            //a hack to get html entities from weird characters
+            "<p onclick=play(songs["+ i +"])>" + $("<div/>").text(songs[i].name).html() + "</p><br>"
+        )
+    }
+    
+    //$("#tracklist").append("<p><a href='songs.zip' target='_blank'> download all</a></p>")
+    
+    document.getElementById("trackbar-container").addEventListener("click", seek);
+
+    if (localStorage.getItem("volume") == "undefined" ||
+        localStorage.getItem("volume") == "NaN") {
+        cachedVolume = 1;
+    } else {
+        cachedVolume = Number(localStorage.getItem("volume"));
+    }
+    console.log("cached volume: " + cachedVolume)
+    updateVolume($('.volume').pageX, cachedVolume)
+})
+
+function setAlbumArt(path) {
+    $("#current-art").attr("src", path);
+}
+
+ function formatTime(seconds) {
+    minutes = Math.floor(seconds / 60);
+    minutes = (minutes >= 10) ? minutes : "0" + minutes;
+    seconds = Math.floor(seconds % 60);
+    seconds = (seconds >= 10) ? seconds : "0" + seconds;
+    return minutes + ":" + seconds;
+  }
+
+
+
+function switchArt(newSong, direction) {
+    var artBlob = "<img src='"+newSong.artPath+"' id='newArt'>";
+    $("art-container").append(artBlob);
+}
+
+//VOLUME BAR
+//volume bar event
+var volumeDrag = false;
+$('.volume').on('mousedown', function (e) {
+    volumeDrag = true;
+    currentSong.muted = false;
+    $('.sound').removeClass('muted');
+    updateVolume(e.pageX);
+});
+$(document).on('mouseup', function (e) {
+    if (volumeDrag) {
+        volumeDrag = false;
+        updateVolume(e.pageX);
+    }
+});
+$(document).on('mousemove', function (e) {
+    if (volumeDrag) {
+        updateVolume(e.pageX);
+    }
+});
+
+var updateVolume = function (x, vol) {
+    var volume = $('.volume');
+    var percentage;
+    //if only volume have specificed
+    //then direct update volume
+    if (vol) {
+        percentage = vol * 100;
+    } else {
+        var position = x - volume.offset().left;
+        percentage = 100 * position / volume.width();
+    }
+
+    if (percentage > 100) {
+        percentage = 100;
+    }
+    if (percentage < 0) {
+        percentage = 0;
+    }
+
+    //update volume bar and sound volume
+    $('.volumeBar').css('width', percentage + '%');
+    currentSong.volume = percentage / 100;
+    console.log("vol to set: " + percentage / 100)
+    localStorage.setItem("volume", percentage / 100)
+};
---
+++ jasmine/songs.js
@@ -0,0 +1,81 @@
+var template = {
+    audioPath : "mp3s/.mp3",
+    artPath : "art/.jpg",
+    name : "",
+    artist : "",
+}
+
+var killvmaim = {
+    audioPath : "mp3s/Grimes - Kill V. Maim.mp3",
+    artPath : "art/artangels.jpg",
+    name : "Kill V. Maim",
+    artist : "Grimes",
+}
+
+var nowheretogo = {
+    audioPath : "mp3s/Nite Jewel - Nowhere To Go.mp3",
+    artPath : "art/goodevening.jpg",
+    name : "Nowhere To Go",
+    artist : "Nite Jewel",
+}
+
+var streetlevel = {
+    audioPath : "mp3s/Neon Indian - Street Level.mp3",
+    artPath : "art/vega.jpg",
+    name : "Street Level",
+    artist : "Neon Indian",
+}
+
+var tracytzu = {
+    audioPath : "mp3s/Carpenter Brut - Looking For Tracy Tzu.mp3",
+    artPath : "art/trilogy.jpg",
+    name : "Looking For Tracy Tzu",
+    artist : "Carpenter Brut",
+}
+
+var nightstalker = {
+    audioPath : "mp3s/Carpenter Brut - Night Stalker.mp3",
+    artPath : "art/nightstalker.jpg",
+    name : "Night Stalker",
+    artist : "Carpenter Brut",
+}
+
+var starroving = {
+    audioPath : "mp3s/Slowdive - Star Roving.mp3",
+    artPath : "art/starroving.jpg",
+    name : "Star Roving",
+    artist : "Slowdive",
+}
+
+var inheaven = {
+    audioPath : "mp3s/Japanese Breakfast - In Heaven.mp3",
+    artPath : "art/inheaven.jpg",
+    name : "In Heaven",
+    artist : "Japanese Breakfast",
+}
+
+var crawling = {
+    audioPath : "mp3s/Bass Drum of Death - Crawling After You.mp3",
+    artPath : "art/crawling.jpg",
+    name : "Crawling After You",
+    artist : "Bass Drum of Death",
+}
+
+var losangeles = {
+    audioPath : "mp3s/X - Los Angeles.mp3",
+    artPath : "art/losangeles.jpg",
+    name : "Los Angeles",
+    artist : "X",
+}
+
+var songs = [
+    killvmaim,
+    streetlevel,
+    nightstalker,
+    starroving,
+    inheaven,
+    crawling,
+    losangeles,
+    tracytzu,
+    nowheretogo,
+]
