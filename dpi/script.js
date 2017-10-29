e50db2ab0614a25b461df2d940787dd745ebd4c1
Adrian Garza <adrianhgarza@gmail.com>
2017-10-28 21:41:54 -0400

    more

--- dpi/script.js
+++ dpi/script.js
@@ -1,16 +1,24 @@
 $(document).ready(function() {
-
+	var ipsIncrementer = setInterval(function() {
+		game.indulgences += game.ips;
+		updateIndulgences();
+	}, 1000)
 })
 
 $("#main-button").click(function() {
 	indulge();
 })
 
-function indulge() {
-	game.indulgences++;
+function updateIndulgences() {
 	$("#indulgences").text(game.indulgences);
 }
 
+function indulge() {
+	game.indulgences++;
+	updateIndulgences();
+}
+
 var game =  {
-	indulgences: 0
+	indulgences: 0,
+	ips: 0
 }
