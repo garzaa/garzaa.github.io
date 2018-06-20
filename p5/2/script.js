a866161ae2c4d94773aec5770a94cf5711e4e22d
Adrian Garza <adrianhgarza@gmail.com>
2018-06-20 10:38:38 -0700

    more processing

---
+++ p5/2/script.js
@@ -0,0 +1,38 @@
+var radius = 300;
+var canvasDiameter = 800;
+
+var speed = 32;
+var magnitude = 10;
+var waveLength = 4;
+var lineWidth = 3;
+
+function setup() {
+	createCanvas(canvasDiameter, canvasDiameter);
+}
+
+function draw() {
+	background("#F5E3E0");
+
+	translate(canvasDiameter/2, canvasDiameter/2);
+
+	stroke("#F9B4ED");
+	strokeCap(ROUND);
+	strokeWeight(1);
+	fill("rgba(0, 0, 0, 0)");
+	
+	var startPixel = -radius;
+	var endPixel = radius;
+
+	console.log(startPixel);
+	
+	for (var i = startPixel+1; i < endPixel; i++) {
+		currentRadius = sqrt((radius * radius) - (i * i));
+		if (i < 0) {
+			currentRadius += sin((frameCount / speed) + (i / waveLength)) * magnitude * pow(abs(i/ 200), 2);
+		} else {
+			currentRadius += sin((-frameCount / speed) + (i / waveLength)) * magnitude * pow(abs(i/ 200), 2);
+		}
+		line(-currentRadius, i, currentRadius, i);
+	}
+
+}
