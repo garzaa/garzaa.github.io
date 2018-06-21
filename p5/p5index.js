da6a0caee03e928f2a59cb1c3b34cf1d505fb29c
Adrian Garza <adrianhgarza@gmail.com>
2018-06-20 18:21:25 -0700

    better p5 sketch index

--- p5/p5index.js
+++ p5/p5index.js
@@ -1,5 +1,7 @@
 $("li").click(function() {
 	numSketch = parseInt($(this).text().split(" ")[0]);
 	$("#p5sketch").attr("src", numSketch + "/index.html");
+	$("li").removeClass("selected-sketch");
+	$(this).addClass("selected-sketch");
 	window.scrollTo(0, 0);
 });
