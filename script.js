ad035d91ab7ce95cb1495a7b15491cd1cf9559e2
garzaa <adrianhgarza@gmail.com>
2018-08-16 19:28:37 -0700

    fixed font issue

--- script.js
+++ script.js
@@ -13,15 +13,15 @@ function setup() {
     cnv.parent("canvas-container")
     
     for (var i=margin + (lineGap % rectWidth) / 2; i<rectWidth-margin; i+=lineGap) {
-		tempLine = [];
-		for (var j=margin + (lineGap % rectHeight) / 2; j<rectHeight-margin; j+=lineGap) {
-			var currPoint = {
-				x: j,
-				y: i
-			};
-			tempLine.push(currPoint);
-		}
-		lines.push(tempLine);
+    tempLine = [];
+    for (var j=margin + (lineGap % rectHeight) / 2; j<rectHeight-margin; j+=lineGap) {
+      var currPoint = {
+        x: j,
+        y: i
+      };
+      tempLine.push(currPoint);
+    }
+    lines.push(tempLine);
     }
 }
 
@@ -33,33 +33,41 @@ function draw() {
     fill('rgb(42, 42, 44)');
     rect(-rectHeight/2, -rectWidth/2, rectHeight, rectWidth);
 
-    fill('rgba(228, 27, 73, 0.3)');
+		stroke('rgba(228, 27, 73, 0.3)');
+		strokeWeight(2);
     for (var i=0; i<lines.length; i++) {
-		for (var j=0; j<lines[i].length; j++) {
+    for (var j=0; j<lines[i].length; j++) {
             var currPoint = lines[i][j];
             var current = createVector(rectHeight/2 - currPoint.x, rectWidth/2 - currPoint.y);
             var towards = createVector(sin(frameCount/32)*64, -cos(frameCount/32)*64).sub(current);
             var newVec = current.add(towards.div(16));
-			ellipse(newVec.x, newVec.y, 3);
-		}
+      createX(newVec.x, newVec.y, 3);
+      
+    }
     }
 
-    fill('rgba(228, 27, 73, 0.5)');
+    stroke('rgba(228, 27, 73, 0.5)');
     for (var i=0; i<lines.length; i++) {
-		for (var j=0; j<lines[i].length; j++) {
+    for (var j=0; j<lines[i].length; j++) {
             var currPoint = lines[i][j];
             var current = createVector(rectHeight/2 - currPoint.x, rectWidth/2 - currPoint.y);
             var towards = createVector(sin(frameCount/32)*64, -cos(frameCount/32)*64).sub(current);
             var newVec = current.add(towards.div(32));
-			ellipse(newVec.x, newVec.y, 3);
-		}
+      createX(newVec.x, newVec.y, 3);
+    }
     }
 
+      /*
     fill('rgb(228, 27, 73)');
     for (var i=0; i<lines.length; i++) {
-		for (var j=0; j<lines[i].length; j++) {
+    for (var j=0; j<lines[i].length; j++) {
             var currPoint = lines[i][j];
-			ellipse(rectHeight/2 - currPoint.x, rectWidth/2 - currPoint.y, 3);
-		}
-    }
+        ellipse(rectHeight/2 - currPoint.x, rectWidth/2 - currPoint.y, 3);
+      }
+    } */
+}
+
+function createX(xcoord, ycoord, width) {
+		line(xcoord-width, ycoord-width, xcoord+width, ycoord+width);
+		line(xcoord-width, ycoord+width, xcoord+width, ycoord-width);
 }
