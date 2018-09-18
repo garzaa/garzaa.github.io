c19c23a43c48ca972c60ef33082df089ebeffa9c
Adrian Garza <adrianhgarza@gmail.com>
2018-09-18 13:12:34 -0700

    Create script.js

---
+++ p5/18/script.js
@@ -0,0 +1,54 @@
+var canvasDiameter = 800;
+var song, analyzer, amp;
+
+var radius = 500;
+var amplitude = 10;
+
+var numShapes = 10;
+var shapeGap = 70;
+
+function preload() {
+	song = new p5.AudioIn();
+  }
+
+function setup() {
+	createCanvas(canvasDiameter, canvasDiameter);
+
+	song.start();
+	
+	// frequency sensor
+	fft = new p5.FFT();
+	fft.setInput(song);
+}
+
+function draw() {
+	background(0);
+	stroke('red');
+	strokeWeight(5);
+	noFill();
+
+	translate(canvasDiameter/2, canvasDiameter/2);
+
+	lvl = song.getLevel();
+
+	for (var j=1; j<=numShapes; j++) {
+		push();
+		rotate(sin((j+1) * frameCount/256) * (j % 2 == 0 ? 1 : -1));
+		print("starting a new shape");
+		// i = number of sides
+		numSides = j + 2
+		beginShape();
+		for (var i=0; i<numSides; i++) {
+			var rot = (i/numSides) * TAU;
+			var rad = j * shapeGap;
+			rad *= log(rad) / 8;
+			rad += lvl * amplitude * j/2;
+			vertex(
+				cos(rot) * rad,
+				sin(rot) * rad
+			);
+		}
+		endShape(CLOSE);
+		pop();
+	}
+}
