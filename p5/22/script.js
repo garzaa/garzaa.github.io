a860c167c728a90e3dc8586032811af411222b5b
garzaa <adrianhgarza@gmail.com>
2018-11-03 16:12:14 -0700

    swapper

--- p5/22/script.js
+++ p5/22/script.js
@@ -1,10 +1,10 @@
-var radius = 400;
+var canvasDiameter = 800;
 
 var bg = "#F7F6C5";
 
 var colors = [
 	"#264653",
-	"#F3FAE1",
+	"rgba(0, 0, 0, 0)",
 	"#2A9D8F",
 	"#E9C46A",
 	"#F4A261",
@@ -13,17 +13,17 @@ var colors = [
 
 var circles = []
 
-var circleDiameter = 150;
-var circleMargin = 50;
+var circleDiameter = 40;
+var circleMargin = 20;
 
-var canvasDiameter = 800;
+var lerpAmount = 0.12;
 
 function setup() {
 	createCanvas(canvasDiameter, canvasDiameter);
 	noStroke();
-	circles.forEach(circle => {
-		circle.run();
-	})
+	colors.forEach(function(c, idx) {
+		circles.push(new SwappableCircle(c, idx))
+	});	
 }
 
 function draw() {
@@ -31,18 +31,17 @@ function draw() {
 	// center
 	translate(canvasDiameter/2, canvasDiameter/2);
 
-	// go to the top left 
-	var numCircles = colors.length;
 	translate(
 		-(circleMargin/2) - (circleDiameter/2),
 		-circleMargin - circleDiameter
-	)
-	for (var i=0; i<colors.length; i++) {
-		push();
-			var coords = getCoords(i);
-			fill(colors[i]);
-			ellipse(coords.x, coords.y, circleDiameter, circleDiameter);
-		pop();
+	);
+
+	circles.forEach(circle => {
+		circle.run();
+	});
+
+	if (frameCount % 80 == 0) {
+		randomSwap();
 	}
 }
 
@@ -60,11 +59,11 @@ function isEven(i) {
 	return i % 2 == 0;
 }
 
-class swappableCircle {
-	constructor(idx, c) {
+class SwappableCircle {
+	constructor(c, idx) {
 		this.c = c;
 		this.idx = idx;
-		this.position = this.originalPosition;
+		this.position = getCoords(idx);
 		this.swapping = false;
 		this.targetPosition = null;
 	}
@@ -72,14 +71,21 @@ class swappableCircle {
 	display() {
 		push();
 			fill(this.c);
-			ellipse(this.position.x, this.position.y, circleDiameter, circleMargin);
+			ellipse(this.position.x, this.position.y, circleDiameter, circleDiameter);
 		pop();
 	}
 
 	update() {
 		if (this.swapping) {
 			//lerp towards target
-			//if difference is less than 1 pixel, just move there	
+			this.position = createVector(
+				lerp(this.position.x, this.targetPosition.x, lerpAmount),
+				lerp(this.position.y, this.targetPosition.y, lerpAmount)
+			)
+			//if difference is less than 1 pixel, just move there
+			if (this.position.dist(this.targetPosition) < 1) {
+				this.position = this.targetPosition;
+			}
 		}
 	}
 
@@ -88,15 +94,31 @@ class swappableCircle {
 		this.display();
 	}
 
-	startSwap(i, initiator=false) {
+	startSwap(otherIdx, initiator=false) {
 		this.swapping = true;
-		this.targetPosition = colors[i].position;
+		this.targetPosition = circles[otherIdx].position;
 		if (initiator) {
-			colors[i].startSwap(this.idx, false);
+			circles[otherIdx].startSwap(this.idx, false);
+			print(circles);
+			print(colors);
 		}
 	}
 }
 
-function randomSwap(i) {
-	var otherIdx = Math.floor(Math.random() * 2 == 0) ? 1 : -1;
+function randomSwap() {
+	var i1 = Math.floor(Math.random() * circles.length);
+	var i2 = 0;
+	if (i1 == 0) {
+		i2 = 1;
+	} else if (i1 == circles.length-1) {
+		i2 = i1-1;
+	} else {
+		if (isEven(i1)) {
+			i2 = i1 + (Math.floor(Math.random() * 2 == 0) ? 1 : -2);
+		} else {
+			i2 = i1 + (Math.floor(Math.random() * 2 == 0) ? 2 : -1);
+		}
+	}
+	print(i1, i2);
+	circles[i1].startSwap(i2, true);
 }
